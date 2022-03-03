0c32e2b0c01dde53b5efef6f788ce53df060368e
kyunghoon <rlarudgns970@naver.com>
2022-03-03 20:57:41 +0900

    .

--- chapter09_02.py
+++ chapter09_02.py
@@ -47,3 +47,31 @@ with open('./resource/test1.csv', 'r') as f:
         for k,v in c.items():
             print(k,v)
         print('-------------')
+
+# 예제4
+w = [[1,2,3],[4,5,6],[7,8,9],[10,11,12],[13,14,15],[16,17,18],[19,20,21]]
+
+with open('./resource/write1.csv', 'w', encoding='utf-8') as f:
+    print(dir(csv))
+    wt = csv.writer(f)
+
+    # dir 확인
+    # print(dir(wt))
+    # 타입 확인
+    # print(type(wt))
+
+    for v in w:
+        wt.writerow(v)
+
+# 예제5
+with open('./resource/write2.csv', 'w', encoding='utf-8') as f:
+    # 필드명
+    fields = ['One', 'Two', 'Three']
+
+    # Dict writer
+    wt = csv.DictWriter(f, fieldnames=fields)
+    # Header Writer
+    wt.writeheader()
+
+    for v in w:
+        wt.writerow({'One': v[0], 'Two' : v[1], 'Three' : v[2]})
