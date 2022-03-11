82900c1db79c5a06ccb93e2a0135544e574c444a
kyunghoon <rlarudgns970@naver.com>
2022-03-11 18:29:38 +0900

    .

---
+++ 220311.py
@@ -0,0 +1,27 @@
+def kyunghoon(a, b):
+    return a + b
+
+
+a = kyunghoon(100, 200)
+print(a)
+
+multiline = '''
+Life is too short
+You need python
+'''
+print(multiline)
+print()
+print('='*10)
+print('my program')
+print('='*10)
+
+b = "Life is too short, you need Python"
+c = b[0:4]
+print(c)
+print()
+
+ab = "I eat %d apples." %4
+print(ab)
+
+cd = "I eat %s apples." %"five"
+print(cd)
