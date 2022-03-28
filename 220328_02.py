ac807e6cf0e69f752415d82611dd5b65cdf8a27c
kyunghoon <rlarudgns970@naver.com>
2022-03-28 22:20:23 +0900

    .

---
+++ 220328_02.py
@@ -0,0 +1,10 @@
+print('='*10, '1번시작', '='*10)
+number = 13
+day = "three"
+a = "I ate {0} apples. So I was sick for {1} days".format(number,day)
+print(a)
+print()
+
+print('='*10, '2번시작', '='*10)
+b = "I bought {num} Apple stocks, down {num2} percent.".format(num=10,num2=4)
+print(b)
