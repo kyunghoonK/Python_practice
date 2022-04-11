355fe02138f00b496fec893daa1f3e4cd55d5bd5
kyunghoon <rlarudgns970@naver.com>
2022-04-11 14:42:18 +0900

    예외 처리

---
+++ 20220411.py
@@ -0,0 +1,15 @@
+menu = ['사과', '바나나', '오렌지']
+
+try:
+    #실행할 코드
+    user_input = int(input("0:사과, 1:바나나, 2:오렌지 >> "))
+    order = menu[user_input]
+except:
+    #예외가 발생할 때 처리할 코드
+    print("잘못된 주문입니다.")
+else:
+    #예외가 발생하지 않고, 정상 실행되었을 때
+    print(f"{order}주스를 주문하였습니다.")
+finally:
+    #항상 실행
+    print("이용해 주셔서 감사합니다.")
