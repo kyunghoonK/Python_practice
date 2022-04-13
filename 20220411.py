5634c664d85a532847afdf36c71d0570a7a57f2a
kyunghoon <rlarudgns970@naver.com>
2022-04-13 23:19:41 +0900

    .

--- 20220411.py
+++ 20220411.py
@@ -1,3 +1,4 @@
+# 예외 처리
 menu = ['사과', '바나나', '오렌지']
 
 try:
@@ -7,6 +8,10 @@ try:
 except:
     #예외가 발생할 때 처리할 코드
     print("잘못된 주문입니다.")
+#except IndexError:
+#    print("메뉴에 없는 번호 입니다.")
+#except ValueError:
+#    print("번호로 입력해 주세요.")   
 else:
     #예외가 발생하지 않고, 정상 실행되었을 때
     print(f"{order}주스를 주문하였습니다.")
