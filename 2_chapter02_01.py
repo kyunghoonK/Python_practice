82ac861c177f32b85e6c54d73e4b72db71e0437c
kyunghoon <rlarudgns970@naver.com>
2022-08-03 15:20:45 +0900

    .

--- 2_chapter02_01.py
+++ 2_chapter02_01.py
@@ -39,8 +39,73 @@ car_detail_list = [
     {'color' : 'silver', 'horsepower' : 300, 'price' : 6000}
 ]
 
+
 del car_company_list[1]
 del car_detail_list[1]
 
 print(car_company_list)
 print(car_detail_list)
+
+print()
+print()
+
+
+# 딕셔너리 구조
+# 코드 반복 지속, 중첩 문제(키), 키 조회 예외 처리 등
+
+car_dicts = [
+    {'car_company' : 'Ferrari', 'car_detail' : {'color' : 'White', 'horsepower' : 400, 'price' : 8000}},
+    {'car_company' : 'Bmw', 'car_detail' : {'color' : 'Black', 'horsepower' : 270, 'price' : 5000}},
+    {'car_company' : 'Audi', 'car_detail' : {'color' : 'silver', 'horsepower' : 300, 'price' : 6000}}
+]
+
+del car_dicts[1]
+print(car_dicts)
+
+print()
+print()
+
+# 클래스 구조
+# 구조 설계 후 재사용성 증가, 코드 반복 최소화, 메소드 활용
+
+class Car():
+    def __init__(self, company, details):
+        self._company = company
+        self._details = details
+    
+    def __str__(self):
+        return 'str : {} - {}'.format(self._company, self._details)
+    
+    def __repr__(self):
+        return 'repr : {} - {}'.format(self._company, self._details)
+
+car1 = Car('Ferrari', {'color' : 'White', 'horsepower' : 400, 'price' : 8000})
+car2 = Car('Bmw', {'color' : 'Black', 'horsepower' : 270, 'price' : 5000})
+car3 = Car('Audi', {'color' : 'silver', 'horsepower' : 300, 'price' : 6000})
+
+print(car1)
+print(car2)
+print(car3)
+
+print(car1.__dict__)
+print(car2.__dict__)
+print(car3.__dict__)
+
+print()
+print()
+
+# 리스트 선언
+car_list = []
+
+car_list.append(car1)
+car_list.append(car2)
+car_list.append(car3)
+
+print(car_list)
+
+print()
+print()
+
+# 반복(__str__)
+for x in car_list:
+    print(repr(x))
