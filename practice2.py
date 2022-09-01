7627d782ae46094baa0db5d61a3e361f56c10b3f
kyunghoon <rlarudgns970@naver.com>
2022-09-01 23:35:19 +0900

    .

--- practice2.py
+++ practice2.py
@@ -1,8 +1,7 @@
-marks = [90, 25, 67, 45, 80]
-number = 0
-for mark in marks:
-    number = number + 1
-    if mark >= 60:
-        print("%d번 학생은 합격입니다." % number)
-    else:
-        print("%d번 학생은 불합격입니다." % number)
+def add(a, b):
+    return a + b
+
+a = 1
+b = 2
+c = add(a, b)
+print(c)
