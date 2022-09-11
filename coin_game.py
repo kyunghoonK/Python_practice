d82e5610bdbc3369624b83eeaebc3f236747048a
kyunghoon <rlarudgns970@naver.com>
2022-09-11 17:27:25 +0900

    .

--- coin_game.py
+++ coin_game.py
@@ -10,23 +10,31 @@ tail = """
 
 print("동전 던지기 게임에 오신 것을 환영합니다.")
 
-computer = random.randint(0,1)
-choice = int(input("앞면일까요? 뒷면일까요? 0:앞면, 1:뒷면 : "))
+keep_playing='yes'
 
-print("동전 결과 : ")
-if computer == 0:
-    print(head)
-else:
-    print(tail)
+while keep_playing == "yes":
+    computer = random.randint(0,1)
+    choice = int(input("앞면일까요? 뒷면일까요? 0:앞면, 1:뒷면 : "))
 
-print("나의 선택 : ")
-if choice == 0:
-    print(head)
-else:
-    print(tail)
+    print("동전 결과 : ")
+    if computer == 0:
+        print(head)
+    else:
+        print(tail)
 
-print("게임 결과 : ")
-if computer == choice :
-    print("적중!!")
-else:
-    print("아쉽네요. 틀렸습니다.")
+    print("나의 선택 : ")
+    if choice == 0:
+        print(head)
+    else:
+        print(tail)
+
+    print("게임 결과 : ")
+    if computer == choice :
+        print("적중!!")
+    else:
+        print("아쉽네요. 틀렸습니다.")
+    
+    keep_playing = input("계속 진행하시겠습니까? yes or no >>> ").lower()
+    print("="*45)
+
+print("동전 던지기 게임이 종류되었습니다.")
