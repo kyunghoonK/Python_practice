1fad8e3f1e9d0b7544086231bb6f6a92848e4ff0
kyunghoon <rlarudgns970@naver.com>
2022-12-24 13:47:59 +0900

    .

--- chapter09_01.py
+++ chapter09_01.py
@@ -27,3 +27,72 @@ print(cts)
 
 # 반드시 close
 f.close()
+
+print()
+print()
+
+# ----- 강의 <File Write(1-2)> -----
+# 예제2
+with open('./resource/it_news.txt', 'r', encoding='UTF-8') as f:
+    c = f.read()
+    print(c)
+    print(iter(c))
+    print(list(c))
+
+print()
+
+# 예제3
+# read() : 전체 읽기, read(10) : 10Byte
+
+with open('./resource/it_news.txt', 'r', encoding='UTF-8') as f:
+    c = f.read(20)
+    print(c)
+    c = f.read(20)
+    print(c)
+    c = f.read(20)
+    print(c)
+    f.seek(0,0)  # 처음으로 돌아가
+    c = f.read(20)
+    print(c)
+
+print()
+
+# 예제4
+# readline : 한 줄씩 읽기
+with open('./resource/it_news.txt', 'r', encoding='UTF-8') as f:
+    line = f.readline()
+    print(line)
+    line = f.readline()
+    print(line)
+
+print()
+
+# 예제5
+# readlines : 전체를 읽은 후 라인 단위 리스트로 저장
+with open('./resource/it_news.txt', 'r', encoding='UTF-8') as f:
+    cts = f.readlines()
+    print(cts)
+    print()
+    for c in cts:
+        print(c, end='')
+
+print()
+
+# -------강의 <File Write(1-3)> -------
+
+# 파일 쓰기(write)
+
+# 예제 1
+with open('./resource/contents1.txt', 'w') as f:
+    f.write('last time\n')
+
+# 예제 2
+with open('./resource/contents1.txt', 'at') as f:
+    f.write('do not miss\n')
+
+
+
+
+
+
+
